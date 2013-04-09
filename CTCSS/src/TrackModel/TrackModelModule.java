f303dade3da4fa6201470aeceb3efa98acac5f22
Main <Main@150.212.61.123>
2013-04-09 14:07:44 -0400

    [Kai] Added some code to my module to allow Nik to display my GUI in the main system

--- CTCSS/src/TrackModel/TrackModelModule.java
+++ CTCSS/src/TrackModel/TrackModelModule.java
@@ -1,5 +1,13 @@
 package TrackModel;
 
 public class TrackModelModule {
-
+private TrackModelPanel gui = null;
+	
+	public TrackModelModule() {
+		gui = new TrackModelPanel();
+	}
+		
+	public TrackModelPanel getPanel() {
+		return gui;
+	}
 }
