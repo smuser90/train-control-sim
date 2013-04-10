a681f580aade2ae4c88a6dcc1edfbffc539afadb
Optimus <Optimus@Optimus-PC>
2013-04-09 21:59:01 -0400

    Added some prototype login features Added Simulator Added CTCTester

---
+++ CTCSS/src/System/Simulator.java
@@ -0,0 +1,13 @@
+package System;
+
+import Log.Log;
+
+public class Simulator implements Runnable{
+	
+	Log log = null;
+	boolean paused = true;
+	
+	public void run() {
+		
+	}
+}
--- CTCSS/src/System/System_GUI.java
+++ CTCSS/src/System/System_GUI.java
@@ -20,6 +20,8 @@ import TrackController.TrackControllerModule;
 import TrackModel.TrackModelModule;
 import CTC.CTCModule;
 import TrainModel.TrainModelModule;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class System_GUI {
 
@@ -33,6 +35,11 @@ public class System_GUI {
 	private static TrackControllerModule trc;
 	private static SystemTime st;
 	private static boolean loggedIn = false;
+	private JPanel panel_1;
+	private JTabbedPane tabbedPane;
+	private JMenuItem mntmLogout;
+	private JMenu mnFile;
+	private JMenuItem mntmLogin;
 	
 	static void renderSplashFrame(int frame) {
         final String[] comps = {"Log", "SystemTime", "CTC", "TrackModel", "TrackController", "TrainModel", "TrainController", "Finalizer"};
@@ -49,8 +56,8 @@ public class System_GUI {
 	private static void setGUILAndF() {
 		try {
             	// Set System L&F
-			UIManager.setLookAndFeel(
-			UIManager.getSystemLookAndFeelClassName());
+			if(UIManager.getSystemLookAndFeelClassName().toString().equals("com.sun.java.swing.plaf.windows.WindowsLookAndFeel"))
+				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 	    } 
 	    catch (UnsupportedLookAndFeelException e) {
 	    	// handle exception
@@ -101,6 +108,7 @@ public class System_GUI {
 		for(int i = 0; i < 5; i++)
 			updateSplash(7);
 		log.append(0, "System Ready\n");
+		log.append(3, "Please Login\n");
 	}
 	
 	/**
@@ -148,57 +156,88 @@ public class System_GUI {
 	private void initialize() {
 		frmCtcss = new JFrame();
 		frmCtcss.setTitle("CTCSS");
-		frmCtcss.setBounds(100, 100, 1000, 600);
+		frmCtcss.setBounds(100, 100, 988, 600);
 		frmCtcss.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmCtcss.getContentPane().setLayout(null);
 		
+		// Log
 		JPanel panel = log.getPanel();
 		panel.setBounds(10, 400, 964, 130);
-		frmCtcss.getContentPane().add(panel);
 		
-		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+			frmCtcss.getContentPane().add(panel);
+		
+		// Tabbed display for modules
+		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.setBackground(Color.GRAY);
 		tabbedPane.setBounds(320, 11, 654, 378);
-		frmCtcss.getContentPane().add(tabbedPane);
+		if(loggedIn)
+			frmCtcss.getContentPane().add(tabbedPane);
 		
+		// Track Model
 		JPanel panel_2 = trm.getPanel();
 		panel_2.setBackground(Color.WHITE);
 		tabbedPane.addTab("Track Model", null, panel_2, null);
 		panel_2.setLayout(null);
 		
+		// Track Controller
 		JPanel panel_3 = trc.getPanel();
 		tabbedPane.addTab("Track Controller", null, panel_3, null);
 		
+		// Train Model
 		JPanel panel_4 = tm.getPanel();
 		panel_4.setBackground(Color.WHITE);
 		tabbedPane.addTab("Train Model", null, panel_4, null);
 		
+		// Train Controller
 		JPanel panel_5 = new JPanel();
 		panel_5.setBackground(Color.WHITE);
 		tabbedPane.addTab("Train Controller", null, panel_5, null);
 		
-		JPanel panel_1 = ctc.getPanel();
+		// CTC
+		panel_1 = ctc.getPanel();
 		panel_1.setBounds(10, 11, 300, 378);
-		frmCtcss.getContentPane().add(panel_1);
+		if(loggedIn)
+			frmCtcss.getContentPane().add(panel_1);
+		
 		
 		JMenuBar menuBar = new JMenuBar();
 		frmCtcss.setJMenuBar(menuBar);
 		
-		JMenu mnFile = new JMenu("File");
+		// File Menu
+		mnFile = new JMenu("File");
 		menuBar.add(mnFile);
 		
-		JMenuItem mntmLogin = new JMenuItem("Login");
+		mntmLogin = new JMenuItem("Login");
+		mntmLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				login();
+			}
+		});
 		mnFile.add(mntmLogin);
 		
-		JMenuItem mntmLogout = new JMenuItem("Logout");
-		mnFile.add(mntmLogout);
+		mntmLogout = new JMenuItem("Logout");
+		mntmLogout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				logout();
+			}
+		});
 		
 		JMenuItem mntmAbout = new JMenuItem("About");
+		mntmAbout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		mnFile.add(mntmAbout);
 		
 		JMenuItem mntmExit = new JMenuItem("Exit");
+		mntmExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
 		mnFile.add(mntmExit);
 		
+		// Simulation Menu
 		JMenu mnSimulation = new JMenu("Simulation");
 		menuBar.add(mnSimulation);
 		
@@ -213,5 +252,23 @@ public class System_GUI {
 		
 		JMenuItem mntmMetrics = new JMenuItem("Metrics");
 		mnSimulation.add(mntmMetrics);
+	}
+	
+	// Simply to show functionality for now will be fleshed out more later
+	public void login() {
+		frmCtcss.getContentPane().add(panel_1);
+		frmCtcss.getContentPane().add(tabbedPane);
+		mnFile.remove(0);
+		mnFile.insert(mntmLogout, 0);
+		frmCtcss.repaint();
+	}
+	
+	// Simply to show functionality for now will be fleshed out more later
+	public void logout() {
+		frmCtcss.remove(panel_1);
+		frmCtcss.remove(tabbedPane);
+		mnFile.remove(0);
+		mnFile.insert(mntmLogin, 0);
+		frmCtcss.repaint();
 	}
 }
