80a2cb770c7f1d87b4969be225dcaae2db3315c7
Optimus <Optimus@Optimus-PC>
2013-04-10 15:29:37 -0400

    Refactored CTCPanel

--- CTCSS/src/CTC/CTCModule.java
+++ CTCSS/src/CTC/CTCModule.java
@@ -1,13 +1,20 @@
 package CTC;
 
+import java.util.ArrayList;
+
 public class CTCModule {
+	// Fields
 	private CTCPanel gui = null;
 	
 	public CTCModule() {
-		gui = new CTCPanel();
+		gui = new CTCPanel(this);
 	}
 		
 	public CTCPanel getPanel() {
 		return this.gui;
+	}
+	
+	protected ArrayList<Integer> getBlockIDs(int line) {
+		return null;
 	}
 }
