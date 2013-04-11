885f1df0e67257e44901b231302da5e95c880065
BADASS69 <sam.musso90@gmail.com>
2013-04-11 13:19:48 -0400

    Updated TrainModel

--- CTCSS/src/TrainModel/TrainModelModule.java
+++ CTCSS/src/TrainModel/TrainModelModule.java
@@ -1,17 +1,44 @@
 package TrainModel;
 
+import TrackModel.Block;
+import java.util.*;
 
 public class TrainModelModule 
 {
-	private TMPanel gui = null;
+	private static TMPanel m_gui = null;
+	private static Map<Integer, TrainModel> m_trainList = null;
+	private int m_trainID;
+	
 	
 	public TrainModelModule()
 	{
-		gui = new TMPanel();
+		m_gui = new TMPanel();
+		m_trainID = 1;
 	}
 	
 	public TMPanel getPanel()
 	{
-		return gui;
+		return m_gui;
+	}
+	
+	public boolean addTrain()
+	{
+		TrainModel train = new TrainModel(m_trainID);
+		m_trainID++;
+		m_trainList
+		return true;
+	}
+	
+	public boolean modifyTrain(int ID)
+	{
+		
+	}
+	
+	public void tick(double timeLapse)
+	{
+		for(int i = 1; i < m_trainID; i++)
+		{
+			m_trainList.get(Integer(i))
+		}
 	}
 }
