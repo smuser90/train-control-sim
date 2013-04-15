63c7ab4945ee576f58a646eaa0e5ff9e222a92c2
Optimus <Optimus@Optimus-PC>
2013-04-15 00:45:38 -0400

    Forgot to add in the LoginDialog Class

---
+++ CTCSS/src/System/LoginDialog.java
@@ -0,0 +1,56 @@
+package System;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import javax.swing.JPasswordField;
+
+public class LoginDialog extends JDialog {
+	private JTextField textField;
+	private JPasswordField passwordField;
+	private boolean loggedInSuccess = false;
+	
+	public LoginDialog(final System_GUI sys) {
+		setTitle("Login");
+		setBounds(100, 100, 200, 125);
+		getContentPane().setLayout(null);
+		
+		JLabel lblUsername = new JLabel("Username:");
+		lblUsername.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblUsername.setHorizontalTextPosition(SwingConstants.RIGHT);
+		lblUsername.setBounds(10, 11, 60, 14);
+		getContentPane().add(lblUsername);
+		
+		textField = new JTextField();
+		textField.setBounds(80, 11, 86, 17);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblPassword.setBounds(10, 36, 60, 14);
+		getContentPane().add(lblPassword);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(80, 36, 86, 17);
+		getContentPane().add(passwordField);
+		
+		JButton btnNewButton = new JButton("Login");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				sys.setLoggedIn(Login.login(textField.getText(), new String(passwordField.getPassword())));
+				setVisible(false);
+			}
+		});
+		btnNewButton.setBounds(20, 61, 146, 23);
+		getContentPane().add(btnNewButton);
+		textField.setColumns(10);
+		setVisible(true);
+	}
+}
